b4c8f233d4294161b77ac0abfd023b0dda4c2e37
Ggame0412 <glb200044@gmail.com>
2026-07-24 00:33:01 +0500

    Update travis_androidsdk.sh add tools

--- android/travis_androidsdk.sh
+++ android/travis_androidsdk.sh
@@ -6,5 +6,10 @@ curl https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -o sdk
 unzip -q sdk.zip
 rm sdk.zip
 yes | ./tools/bin/sdkmanager --licenses
+yes | ./tools/bin/sdkmanager \
+    "platform-tools" \
+    "platforms;android-29" \
+    "build-tools;29.0.2" \
+    "ndk;21.4.7075529"
 echo sdk.dir=`pwd` >> ../local.properties
 cd ..
