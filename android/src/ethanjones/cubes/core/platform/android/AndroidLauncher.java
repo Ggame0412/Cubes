c9001ff56572a2b882ee261795e140d45852f118
Ggame0412 <glb200044@gmail.com>
2026-08-05 23:29:27 +0500

    Update AndroidLauncher.java Adding a question about strict file permissions

--- android/src/ethanjones/cubes/core/platform/android/AndroidLauncher.java
+++ android/src/ethanjones/cubes/core/platform/android/AndroidLauncher.java
@@ -3,26 +3,64 @@ package ethanjones.cubes.core.platform.android;
 import ethanjones.cubes.core.platform.Compatibility;
 import ethanjones.cubes.core.platform.Launcher;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
 public class AndroidLauncher extends AndroidApplication implements Launcher {
 
+  private static final int PERMISSION_REQUEST_CODE = 101;
+  private boolean cubesInitialized = false;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    if (hasStoragePermission()) {
+      initCubes();
+    } else {
+      requestPermissions(
+          new String[]{
+              Manifest.permission.WRITE_EXTERNAL_STORAGE,
+              Manifest.permission.READ_EXTERNAL_STORAGE
+          },
+          PERMISSION_REQUEST_CODE
+      );
+    }
+  }
+
+  private boolean hasStoragePermission() {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+      return true; // На Android 5.1 и ниже права выдаются при установке
+    }
+    return checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        == PackageManager.PERMISSION_GRANTED;
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    if (requestCode == PERMISSION_REQUEST_CODE) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        initCubes();
+      } else {
+        finish();
+      }
+    }
+  }
+
+  private void initCubes() {
+    if (cubesInitialized) return;
+    cubesInitialized = true;
     new AndroidCompatibility(this).startCubes();
   }
 
   @Override
   public void onTrimMemory(int level) {
     super.onTrimMemory(level);
-    //if (level == TRIM_MEMORY_RUNNING_MODERATE || level == TRIM_MEMORY_RUNNING_LOW) {
-    //  Debug.lowMemory();
-    //} else if (level == TRIM_MEMORY_RUNNING_CRITICAL) {
-    //  Debug.criticalMemory();
-    //}
   }
 
   @Override
